4db1d523c851afc235e708f363be4021c5c3eb3c
softsuregrytutor <clangtutor@gmail.com>
2024-02-25 04:53:26 -0800

    ref(init person table & populate)

--- Application.java
+++ Application.java
@@ -4,25 +4,38 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Personne p = new Personne();
+        int n;
+        System.out.println("Nombre de Personne?");
+        n = scanner.nextInt();
 
-        System.out.println("ID ?");
-        p.id = scanner.nextInt();
+        Personne T[] = new Personne[n];
 
-        System.out.println("NOM ?");
-        p.nom = scanner.next();
+        for(int i=0 ; i<n ; i++) {
+            T[i] = new Personne();
 
-        System.out.println("PRENOM ?");
-        p.prenom = scanner.next();
+            System.out.println("Peronne Numero " + (i+1));
 
-        System.out.println("Email ?");
-        p.email = scanner.next();
+            System.out.println("ID ?");
+            T[i].id = scanner.nextInt();
+    
+            System.out.println("NOM ?");
+            T[i].nom = scanner.next();
+    
+            System.out.println("PRENOM ?");
+            T[i].prenom = scanner.next();
+    
+            System.out.println("Email ?");
+            T[i].email = scanner.next();
+    
+            System.out.println("Date ?");
+            T[i].date = scanner.next();
+        }      
 
-        System.out.println("Date ?");
-        p.date = scanner.next();
+        for(int i=0; i<n ; i++){
+            System.out.println("Peronne Numero " + (i+1));
+            System.out.println(T[i]);
+        }
 
-        System.out.println(p);
-        
         scanner.close();
     }
 }
--- Personne.java
+++ Personne.java
@@ -1,13 +1,13 @@
 public class Personne {
     //attribut - champs
-    int id;
-    String nom;
-    String prenom;
-    String email;
-    String date; //jj-mm-aaaa
+    int id = -1;
+    String nom = "";
+    String prenom = "";
+    String email = "@";
+    String date = "jj/mm/aaaa"; //jj-mm-aaaa
 
     //méthodes
-    
+
     @Override
     public String toString() {
         return "Nom = " + nom + " Prenom = " + prenom + " Email = " + email + " Date = " + date;
